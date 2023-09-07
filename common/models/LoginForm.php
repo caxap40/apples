192d09024856217a2a7e926f7f5b7bd4f72449b6
Skorykh <sii200708@gmail.com>
2023-09-07 13:46:29 +0700

    Упрощение входа (только по паролю). Русификация

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -10,7 +10,7 @@ use yii\base\Model;
  */
 class LoginForm extends Model
 {
-    public $username;
+    public $username = 'demo';
     public $password;
     public $rememberMe = true;
 
@@ -24,7 +24,7 @@ class LoginForm extends Model
     {
         return [
             // username and password are both required
-            [['username', 'password'], 'required'],
+            [[/*'username',*/ 'password'], 'required', 'message'=>'Пароль не может быть пустым'],
             // rememberMe must be a boolean value
             ['rememberMe', 'boolean'],
             // password is validated by validatePassword()
@@ -44,7 +44,7 @@ class LoginForm extends Model
         if (!$this->hasErrors()) {
             $user = $this->getUser();
             if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
+                $this->addError($attribute, 'Неверный пароль.');
             }
         }
     }
